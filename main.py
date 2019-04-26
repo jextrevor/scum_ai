71a628769beaef86b1c3877d56f4cadbfae654d1
Trevor Jex <jextrevor@gmail.com>
2019-04-25 22:16:29 -0600

    add comments with thoughts

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 print("Welcome to the scum AI!")
+
+#If the AI keeps track of what has been played by whom, and when someone has passed, it can make decent guesses about what cards people do have. It can also determine how many cards of a certain kind are still available.
+#The AI should try to execute the chain of events that will make it win. It could be configured to be self-learning.
+#The problem with having an AI play against humans is that humans are very nuanced, and hard to predict. The AI must not rely on any specific human behavior (although it may be biased towards a certain chain of events), it should rather rely on stats to choose the best possible outcome.
+#The AI should be able to work with a limited set of data, but it's unclear how it will handle a limited set.
